13096910c72e0b5787badf8b674098cccd6144f3
Ayush Raj <rajayush0822@gmail.com>
2020-06-11 13:36:50 +0530

    Typed all the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work in harmony where the first one sets and gets the data.
+## And the second one checks the disc or stored data and gets the inverse.
 
-## Write a short comment describing this function
+## This function creates a list of functions that help in setting and getting
+## the main matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function()	 i
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function checks that if the inverse is already calculated.
+## If yes, then it gets the inverse from the disk, otherwise it calculates the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting inverse of matrix")
+    return(i)
+  }
+  data <- x$get
+  i <- solve(data)
+  x$setinv(i)
+  i
 }
+
+
+
